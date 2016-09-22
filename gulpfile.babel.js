a7f2a3535cbc02af63597c3eda2d85fb7129dd7a
allenzeng <zengjun_cm@sina.com>
2016-09-22 22:32:16 +0800

    调整gulp配置文件

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,29 +20,28 @@ gulp.task('serve', () => {
     },
   });
 
-  gulp.watch('src/*.js', ['js', browserSync.reload]);
-  gulp.watch('src/*.html', ['html', browserSync.reload]);
-  gulp.watch('src/*.css', ['css', browserSync.reload]);
+  gulp.watch('src/**/*.js', ['js', browserSync.reload]);
+  gulp.watch('src/**/*.html', ['html', browserSync.reload]);
+  gulp.watch('src/**/*.css', ['css', browserSync.reload]);
 });
 
-gulp.task('js', () => gulp.src('src/*.js')
+gulp.task('js', () => gulp.src('src/**/*.js')
     .pipe(babel({
       presets: ['es2015'],
     }))
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.stream()));
 
-gulp.task('html', () => gulp.src('src/*.html')
+gulp.task('html', () => gulp.src('src/**/*.html')
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.stream()));
 
-gulp.task('css', () => gulp.src('src/*.css')
+gulp.task('css', () => gulp.src('src/**/*.css')
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.stream()));
 
-gulp.task('test', () => gulp.src('test/*')
-    .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.stream()));
+gulp.task('img', () => gulp.src('public/*.JPG')
+  .pipe(gulp.dest('dist/')));
 
 gulp.task('clean', () => del(['dist']));
 
@@ -54,4 +53,4 @@ gulp.task('build', ['clean'], () => gulp.src('src/*.js')
     .pipe(hash())
     .pipe(gulp.dest('dist/')));
 
-gulp.task('default', ['clean', 'js', 'test', 'serve']);
+gulp.task('default', ['clean', 'js', 'html', 'css', 'img', 'serve']);
